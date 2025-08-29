f2576a963e7834c470ca507addcde39843e4a35d
Brian Olson <beolson@hero4hire.com>
2025-08-29 14:14:34 -0500

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+  basePath: "/www-hero4hire-com",
 };
 
 export default nextConfig;
