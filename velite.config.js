0b87ee8dc8ba26f43efb9c4afa2d4670ad1dc34f
Brian Olson <beolson@hero4hire.com>
2025-09-05 13:33:20 -0500

    update

--- velite.config.js
+++ velite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, s } from 'velite'
+import { defineConfig, s } from "velite";
 
 // `s` is extended from Zod with some custom schemas,
 // you can also import re-exported `z` from `velite` if you don't need these extension schemas.
@@ -8,21 +8,20 @@ export default defineConfig({
 
   collections: {
     posts: {
-      name: 'Post', // collection type name
-      pattern: 'posts/**/*.mdx', // content files glob pattern
+      name: "Post", // collection type name
+      pattern: "posts/**/*.mdx", // content files glob pattern
       schema: s
         .object({
           title: s.string().max(99), // Zod primitive type
           //slug: s.slug('posts'), // validate format, unique in posts collection
           slug: s.path(), // auto generate slug from file path
           date: s.isodate(), // input Date-like string, output ISO Date string.
-    
+
           excerpt: s.excerpt(), // excerpt of markdown content
-          content: s.markdown() // transform markdown to html
+          content: s.markdown(), // transform markdown to html
         })
         // more additional fields (computed fields)
-        .transform(data => ({ ...data, permalink: `/blog/${data.slug}` }))
+        .transform((data) => ({ ...data, permalink: `/blog/${data.slug}` })),
     },
-
-  }
-})
+  },
+});
